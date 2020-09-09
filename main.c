afe8df3b126a4a11b57a53a42bd89cebc0ca0e61
ankien <52425502+ankien@users.noreply.github.com>
2020-09-08 20:39:33 -0400

    my largest if-statement ever

--- main.c
+++ main.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define HASHSIZE 200
+#include <stdint.h>
 
 struct Node {
     char* symbol;
-    unsigned int address;
-    unsigned int order;
+    uint16_t address;
 };
+
+#define HASHSIZE (32768U / sizeof(struct Node)) // 2^15 bytes / size of symbol + address
 
 struct Node* hashArray[HASHSIZE];
 
-unsigned int hashFunction(char* symbol) {
-    unsigned int hash = 0;
-    for(unsigned int i = 0; i < strlen(symbol); i++) {
+uint16_t hashFunction(char* symbol) {
+    uint64_t hash = 0;
+    for(uint64_t i = 0; i < strlen(symbol); i++) {
         hash += symbol[i];
     }
-    return hash % HASHSIZE;
+    return (hash * HASHSIZE) % HASHSIZE;
 }
 
-void insertNode(char* symbol, unsigned int address, unsigned int order) {
-    unsigned int index = hashFunction(symbol);
+void insertNode(char* symbol, uint16_t address) {
+    uint16_t index = hashFunction(symbol);
     struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
-    newNode->symbol = symbol;
+    newNode->symbol = (char*)malloc(strlen(symbol) + 1);
+    strcpy(newNode->symbol,symbol);
     newNode->address = address;
-    newNode->order = order;
     while(!hashArray[index]) {
         index = (index + 1) % HASHSIZE;
     }
@@ -33,7 +33,7 @@ void insertNode(char* symbol, unsigned int address, unsigned int order) {
 }
 
 struct Node* findNode(char* symbol) {
-    unsigned int index = hashFunction(symbol);
+    uint16_t index = hashFunction(symbol);
     while(hashArray[index]) {
         if(strcmp(hashArray[index]->symbol, symbol) == 0)
             return hashArray[index];
@@ -42,15 +42,7 @@ struct Node* findNode(char* symbol) {
     return NULL;
 }
 
-void inOrderTraversal(unsigned int count) {
-    struct Node* temp;
-    for(int i = 0; i < count; i++) {
-        temp = findNode(count);
-        printf("%s %X\n",temp->symbol,temp->address);
-    }
-}
-
-int stringHasSpecialChars(char* string) {
+uint8_t stringHasSpecialChars(char* string) {
     for(int i = 0; i < strlen(string); i++) {
         if((string[i] == ' ') || (string[i] == '$') || (string[i] == '!') || (string[i] == '=') ||
            (string[i] == '+') || (string[i] == '-') || (string[i] == '(') || (string[i] == ')') || (string[i] == '@'))
@@ -59,11 +51,31 @@ int stringHasSpecialChars(char* string) {
     return 0;
 }
 
-void printError(char* line,unsigned int lineNumber, char* error) {
-    printf("<%s>\nLine<%d><%s>\n",line,lineNumber,error);
+uint8_t checkIfOpcode(char* string) { // Better performance could be achieved if you switched on the first character of the string
+    if((strcmp(string,"ADD") == 0) || (strcmp(string,"ADDF") == 0) || (strcmp(string,"ADDR") == 0) || (strcmp(string,"AND") == 0) ||
+       (strcmp(string,"CLEAR") == 0) || (strcmp(string,"COMP") == 0) || (strcmp(string,"COMPF") == 0) || (strcmp(string,"COMPR") == 0) ||
+       (strcmp(string,"DIV") == 0) || (strcmp(string,"DIVF") == 0) || (strcmp(string,"DIVR") == 0) || (strcmp(string,"FIX") == 0) ||
+       (strcmp(string,"FLOAT") == 0) || (strcmp(string,"HIO") == 0) || (strcmp(string,"J") == 0) || (strcmp(string,"JEQ") == 0) ||
+       (strcmp(string,"JGT") == 0) || (strcmp(string,"JLT") == 0) || (strcmp(string,"JSUB") == 0) || (strcmp(string,"LDA") == 0) ||
+       (strcmp(string,"LDB") == 0) || (strcmp(string,"LDCH") == 0) || (strcmp(string,"LDF") == 0) || (strcmp(string,"LDL") == 0) ||
+       (strcmp(string,"LDS") == 0) || (strcmp(string,"LDT") == 0) || (strcmp(string,"LDX") == 0) || (strcmp(string,"LPS") == 0) ||
+       (strcmp(string,"MUL") == 0) || (strcmp(string,"MULF") == 0) || (strcmp(string,"MULR") == 0) || (strcmp(string,"NORM") == 0) ||
+       (strcmp(string,"OR") == 0) || (strcmp(string,"RD") == 0) || (strcmp(string,"RMO") == 0) || (strcmp(string,"RSUB") == 0) ||
+       (strcmp(string,"SHIFTL") == 0) || (strcmp(string,"SHIFTR") == 0) || (strcmp(string,"SIO") == 0) || (strcmp(string,"SSK") == 0) ||
+       (strcmp(string,"STA") == 0) || (strcmp(string,"STB") == 0) || (strcmp(string,"STCH") == 0) || (strcmp(string,"STF") == 0) ||
+       (strcmp(string,"STI") == 0) || (strcmp(string,"STL") == 0) || (strcmp(string,"STS") == 0) || (strcmp(string,"STSW") == 0) ||
+       (strcmp(string,"STT") == 0) || (strcmp(string,"STX") == 0) || (strcmp(string,"SUB") == 0) || (strcmp(string,"SUBF") == 0) ||
+       (strcmp(string,"SUBR") == 0) || (strcmp(string,"SVC") == 0) || (strcmp(string,"TD") == 0) || (strcmp(string,"TIO") == 0) ||
+       (strcmp(string,"TIX") == 0) || (strcmp(string,"TIXR") == 0) || (strcmp(string,"WD") == 0))
+        return 0;
+    return 1;
 }
 
-int main(unsigned int argc, char* argv[]) {
+void printError(char* line,uint64_t lineNumber, char* error) {
+    printf("<%s>\nLine<%u><%s>\n",line,lineNumber,error);
+}
+
+int8_t main(uint8_t argc, char* argv[]) {
     /* SIC Specifications */
     /* Memory */
     // 8-bit bytes, 3-byte words, 32K byte limit (2^15 bytes)
@@ -98,15 +110,16 @@ int main(unsigned int argc, char* argv[]) {
     }
 
     FILE* inputFile = fopen(argv[1],"r");
-    if(!inputFile) { // If pointer to buffer is null
+    if(!inputFile) {
         printf("Error in opening file\n");
         return 1;
     }
 
     // Pass 1
-    unsigned int lineCount = 0;
-    unsigned int address = 0;
-    unsigned int symbolCount = 0;
+    uint64_t lineCount = 0;
+    uint16_t address = 0;
+    char symbol[6];
+    uint8_t symbolFlag = 0;
     char line[1024];
     while(fgets(line,1024,inputFile)) {
         lineCount++;
@@ -118,26 +131,51 @@ int main(unsigned int argc, char* argv[]) {
             if((line[0] >= 'A') && (line[0] <= 'Z')) { // Read symbol
                 if(findNode(token) != NULL) {
                     printError(line,lineCount,"Duplicate symbol");
+                    return 1;
                 } else if((strcmp(token,"START") == 0 ) || (strcmp(token,"END") == 0 ) || (strcmp(token,"BYTE") == 0 ) ||
                           (strcmp(token,"WORD") == 0 ) || (strcmp(token,"RESB") == 0 ) || (strcmp(token,"RESW") == 0 ) ||
                           (strcmp(token,"RESR") == 0 ) || (strcmp(token,"EXPORTS") == 0 )) {
                     printError(line,lineCount,"Symbol = directive name");
+                    return 1;
                 } else if(strlen(token) > 6) {
                     printError(line,lineCount,"Symbol cannot be longer than 6 characters");
+                    return 1;
                 } else if(stringHasSpecialChars(token) != 0) {
                     printError(line,lineCount,"Symbol cannot contain spaces, $, !, =, +, -, (, ), @");
+                    return 1;
                 } else {
-                    insertNode(token,address,symbolCount++);
+                    strcpy(symbol,token);
+                    symbolFlag = 1;
+                    insertNode(token,address);
                 }
             }
             
             while(token) {
-                
+                if(strcmp(token, "START") == 0) {
+                    address = atoi(strtok(NULL," \t"));
+                    break;
+                } else {
+                    address = 0;
+                }
+
+                if(checkIfOpcode(token) == 0) {
+                    
+                } else if() {
+                    
+                } else {
+                    printError(line,lineCount,"Invalid operation code");
+                    return 1;
+                }
+
                 token = strtok(NULL," \t"); // will be null or the next token
             }
+
+            if(symbolFlag == 1) { printf("%s %X\n",symbol,address); symbolFlag = 0; } // print symbol and the address it's located at
+        } else {
+            printError(line,lineCount,"Empty line");
+            return 1;
         }
     }
-    inOrderTraversal(symbolCount);
 
     fclose(inputFile);
     return 0;
